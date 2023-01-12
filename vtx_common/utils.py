ec6f62b4bf435aaaf7f34966691b702e88371d41
vEpiphyte <epiphyte@vertex.link>
2023-01-12 16:16:41 -0500

    Add optional mailgun support to github_release (#9)
    * Add optional mailgun support to github_release

--- vtx_common/utils.py
+++ vtx_common/utils.py
@@ -1,8 +1,12 @@
 import os
+import logging
 import subprocess
 
+import requests
 
-from typing import ByteString, AnyStr
+from typing import List, AnyStr
+
+logger = logging.getLogger(__name__)
 
 def system(*args, **kwargs) -> bytes:
     '''Simple subprocess wrapper'''
@@ -19,3 +23,24 @@ def reqGitDir(cwd: AnyStr =None) -> bool:
     if os.path.isdir(gitdir):
         return True
     return False
+
+def sendMailMessage(dest: List[AnyStr],
+                    sender: AnyStr,
+                    text: AnyStr,
+                    subject: AnyStr,
+                    domain: AnyStr,
+                    token=AnyStr) -> bool:
+    url = f'https://api.mailgun.net/v3/{domain}/messages'
+    payload = {
+        'from': sender,
+        'to': ','.join(dest),
+        'subject': subject,
+        'text': text,
+    }
+    auth = requests.auth.HTTPBasicAuth('api', token)
+    resp = requests.post(url, auth=auth, data=payload)
+    if resp.status_code != 200:
+        logger.error(f'Failed to send email: {resp}')
+        return False
+    logger.info('Sent email :)')
+    return True
